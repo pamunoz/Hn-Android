4f6e336d9510a84ebbfe37e3382ec9f4199122b9
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 11:05:32 -0300

    Add the principal packages

--- app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.hnandroid
+package com.example.hnandroid.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hnandroid.R
 
 class MainActivity : AppCompatActivity() {
 
