11e35b831b0bc7966719bf3536b25584c774500a
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 19:47:46 -0300

    Add recyclerview adapter structure

---
+++ app/src/main/java/com/example/hnandroid/adapter/HnStoriesAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.hnandroid.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class HnStoriesAdapter: RecyclerView.Adapter<HnStoriesAdapter.StoriesHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoriesHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: StoriesHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class StoriesHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+}
