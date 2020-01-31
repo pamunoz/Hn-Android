04fadd3e61b60c5e813ff19caf57fc2953cf7d82
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 19:41:40 -0300

    Add the view model to the main activity

--- app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hnandroid.R
 import com.example.hnandroid.ui.base.BaseActivity
+import com.example.hnandroid.ui.viewmodule.HnStoriesViewModel
+import com.example.hnandroid.utils.getViewModel
 
 class MainActivity : BaseActivity() {
 
-    private val hnStoriesViewModel by lazy {  }
+    private val hnStoriesViewModel by lazy { getViewModel<HnStoriesViewModel>() }
 
+    /**
+     * Starting point of the activity
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
