cb3b46b9531453ca13cc630d9bfd0a8d9ec21a91
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 16:24:39 -0300

    Add the provider module for the Hacker News Service

--- app/src/main/java/com/example/hnandroid/api/HnService.kt
+++ app/src/main/java/com/example/hnandroid/api/HnService.kt
@@ -7,7 +7,7 @@ import retrofit2.http.GET
  * Fetch all the latest news article from various news services
  * using the News API.
  */
-interface NewsService {
+interface HnService {
 
     /**
      * Retrieves all the latest news article from Google news using News API.
