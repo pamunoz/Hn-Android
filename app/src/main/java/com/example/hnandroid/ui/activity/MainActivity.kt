75b24d5c8273070e76912c41c49ecfda1c7179c5
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 20:37:05 -0300

    Add thelayout for the recyclerview

--- app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.hnandroid.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.hnandroid.R
 import com.example.hnandroid.ui.base.BaseActivity
 import com.example.hnandroid.ui.viewmodule.HnStoriesViewModel
 import com.example.hnandroid.utils.getViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     private val hnStoriesViewModel by lazy { getViewModel<HnStoriesViewModel>() }
+
+    private lateinit var linearLayoutManager: LinearLayoutManager
 
     /**
      * Starting point of the activity
@@ -17,5 +20,8 @@ class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        linearLayoutManager = LinearLayoutManager(this)
+        rv_stories.layoutManager = linearLayoutManager
     }
 }
