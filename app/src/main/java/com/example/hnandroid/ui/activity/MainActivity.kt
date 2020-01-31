8d7d8d0449f85bfc88d4ca314fb8233859a78bda
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 19:05:07 -0300

    Start with the main activity

--- app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/hnandroid/ui/activity/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.hnandroid.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hnandroid.R
+import com.example.hnandroid.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    private val hnStoriesViewModel by lazy {  }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
