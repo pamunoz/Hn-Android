e74ff318e308eaa9ec1815e1c23e86bbafa86eea
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 20:08:40 -0300

    Add row layout to the adapter

--- app/src/main/java/com/example/hnandroid/adapter/HnStoriesAdapter.kt
+++ app/src/main/java/com/example/hnandroid/adapter/HnStoriesAdapter.kt
@@ -3,17 +3,25 @@ package com.example.hnandroid.adapter
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hnandroid.R
+import com.example.hnandroid.model.HnStories
 import com.example.hnandroid.model.HnStoriesAdapterEvent
+import com.example.hnandroid.utils.inflate
 
 class HnStoriesAdapter(
     private val listener: (HnStoriesAdapterEvent) -> Unit
 ): RecyclerView.Adapter<HnStoriesAdapter.StoriesHolder>() {
 
+    /**
+     * List of hacker news tories
+     */
+    private var hnStories: List<HnStories> = emptyList()
 
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StoriesHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    /**
+     * Inflate the view
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = StoriesHolder(parent.inflate(
+        R.layout.row_hnstory))
 
     override fun getItemCount(): Int {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
