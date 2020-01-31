b01ea0556daaab79f2ff18c854c96067617bec36
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 17:24:39 -0300

    Start the repository

---
+++ app/src/main/java/com/example/hnandroid/repo/HnRepository.kt
@@ -0,0 +1,10 @@
+package com.example.hnandroid.repo
+
+import javax.inject.Inject
+
+/**
+ * Repository abstracts the logic of fetching the data and persisting it for
+ * offline.
+ */
+// TODO: finish this repository with the remote and local logic.
+class HnRepository @Inject constructor()
