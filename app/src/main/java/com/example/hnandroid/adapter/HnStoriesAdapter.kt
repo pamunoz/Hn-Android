949fdb371f798c2e582af97d56b7ae7a460f1366
pamunoz <pfariasmunoz@gmail.com>
2020-01-31 20:16:49 -0300

    Add holder to the adapter and swap data method

--- app/src/main/java/com/example/hnandroid/adapter/HnStoriesAdapter.kt
+++ app/src/main/java/com/example/hnandroid/adapter/HnStoriesAdapter.kt
@@ -7,6 +7,7 @@ import com.example.hnandroid.R
 import com.example.hnandroid.model.HnStories
 import com.example.hnandroid.model.HnStoriesAdapterEvent
 import com.example.hnandroid.utils.inflate
+import kotlinx.android.synthetic.main.row_hnstory.view.*
 
 class HnStoriesAdapter(
     private val listener: (HnStoriesAdapterEvent) -> Unit
@@ -23,15 +24,37 @@ class HnStoriesAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = StoriesHolder(parent.inflate(
         R.layout.row_hnstory))
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
+    /**
+     * Bind the view with the data
+     */
     override fun onBindViewHolder(holder: StoriesHolder, position: Int) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
     class StoriesHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
+        /**
+         * Binds the UI with the data and handles clicks
+         */
+        fun bind(hnStory: HnStories, listener: (HnStoriesAdapterEvent) -> Unit) = with(itemView) {
+            tv_story_title.text = hnStory.storyTitle
+            val footText = "${hnStory.author} - ${hnStory.createdAt}"
+            tv_foot.text = footText
+            setOnClickListener { listener(HnStoriesAdapterEvent.ClickEvent) }
+        }
+    }
+
+    /**
+     * Swap function to set new data on updating
+     */
+    fun replaceItems(items: List<HnStories>) {
+        hnStories = items
+        notifyDataSetChanged()
     }
 }
